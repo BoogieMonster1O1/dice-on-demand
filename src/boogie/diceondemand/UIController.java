b71b3eac89327e7c40348f6ea799494526959643
BoogieMonster1O1 <shrish.deshpande@icloud.com>
2020-04-23 17:20:41 +0530

    Minor tweaks

--- src/boogie/diceondemand/UIController.java
+++ src/boogie/diceondemand/UIController.java
@@ -7,24 +7,28 @@ import java.awt.event.ActionListener;
 
 public class UIController{
     JFrame frame = new JFrame();
+    JLabel fieldtext = new JLabel("Faces on Dice:-");
     JLabel heading = new JLabel("Dice on Demand");
     JButton clicker = new JButton("Roll");
-    JTextField field = new JTextField();
+    JTextField field = new JTextField("6");
 
     public UIController(){
 
-        heading.setBounds(135,0,180,90);
+        heading.setBounds(105,0,240,30);
         clicker.setBounds(135,65,180,90);
+        fieldtext.setBounds(105,190,240,30);
         field.setBounds(150,220,150,30);
 
         heading.setFont(new java.awt.Font("Arial", Font.BOLD,28));
         clicker.setFont(new java.awt.Font("Arial", Font.BOLD,24));
         field.setFont(new java.awt.Font("Arial", Font.PLAIN,18));
+        fieldtext.setFont(new java.awt.Font("Arial", Font.PLAIN,18));
 
         frame.add(field);
         frame.add(clicker);
         frame.add(heading);
-        frame.setTitle("CPS Calculator");
+        frame.add(fieldtext);
+        frame.setTitle("Dice on Demand");
         frame.setResizable(false);
         frame.setSize(450,300);
         frame.setLayout(null);
@@ -36,6 +40,7 @@ public class UIController{
             public void actionPerformed(ActionEvent e) {
             }
         });
+        clicker.setBackground(Color.CYAN);
 
         field.setColumns(4);
 
